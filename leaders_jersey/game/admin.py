637b1826eca5a902ae1b1ffbeca839906ae33b95
Jan Lodewijk <160859694+janlodewijk@users.noreply.github.com>
2025-03-24 22:41:13 +0100

    nothing

--- leaders_jersey/game/admin.py
+++ leaders_jersey/game/admin.py
@@ -3,8 +3,14 @@ from .models import Rider, Race, Stage, StageResult
 
 # Register your models here.
 
-admin.site.register(Rider)
+
 admin.site.register(Race)
+
+@admin.register(Rider)
+class RiderAdmin(admin.ModelAdmin):
+    list_display = ('start_number', 'rider_name', 'team', 'nationality', 'external_id', 'is_participating')
+    list_filter = ('team', 'nationality')
+    ordering = ('start_number',)
 
 @admin.register(Stage)
 class StageAdmin(admin.ModelAdmin):
