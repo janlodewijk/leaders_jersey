0ddd1c23a22d72649d004de6569206dff3dd77db
Jan Lodewijk <160859694+janlodewijk@users.noreply.github.com>
2025-04-15 21:37:43 +0200

    configured ETL logging and added logger to extract startlist

--- leaders_jersey/etl/extract/extract_startlist.py
+++ leaders_jersey/etl/extract/extract_startlist.py
@@ -1,6 +1,13 @@
 from procyclingstats import RaceStartlist
+from etl.logging_config import logger
 
 def extract_startlist(race, year):
-    url = f"https://www.procyclingstats.com/race/{race}/{year}/startlist"
-    startlist = RaceStartlist(url)
-    return startlist.parse()
+    try:
+        url = f"https://www.procyclingstats.com/race/{race}/{year}/startlist"
+        startlist = RaceStartlist(url)
+        data = startlist.parse()
+        logger.info(f"Startlist successfully extracted for {race} ({year}) with {len(data)} riders")
+        return data
+    except Exception as e:
+        logger.error(f"Something went wrong while extrcting data for {race} ({year}): {e}")
+        return None
